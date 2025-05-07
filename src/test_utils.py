f7c4cbf92f18bfdf7b1f1e4cfc9d5b696ca47123
llebel <loic.lebel@gmail.com>
2025-05-07 12:46:39 +0000

    Implement extract markdown images and links  functions

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from utils import split_nodes_delimiter
+from utils import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestUtils(unittest.TestCase):
     def test_split_nodes_delimiter(self):
@@ -26,3 +26,15 @@ class TestUtils(unittest.TestCase):
         ]
         self.assertEqual(len(result), len(expected))
         self.assertEqual(result, expected)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -18,4 +19,22 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 odd = not odd
 
     return new_nodes
-                
+
+def extract_markdown_images(text):
+    images = []
+    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    for match in matches:
+        alt_text = match[0]
+        url = match[1]
+        images.append((alt_text, url))
+    return images
+
+def extract_markdown_links(text):
+    links = []
+    matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    for match in matches:
+        link_text = match[0]
+        url = match[1]
+        links.append((link_text, url))
+    return links
+        
